ff89d7f1d38fe689ec5be9bef92dfb87397c68ee
Ankit Mishra <90400569+Ankit-Mishra07@users.noreply.github.com>
2022-02-13 20:00:19 +0530

    Started working on it

---
+++ client/src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Header.css'
+import {Link} from 'react-router-dom'
+const Header = () => {
+  return (
+    <div>
+      <Link>Intuitive Quiz Hub</Link>
+    </div>
+  )
+}
+
+export default Header
