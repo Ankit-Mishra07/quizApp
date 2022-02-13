9c570f451f5cbda6cb8f6f0fab7315c204fb906a
Ankit Mishra <90400569+Ankit-Mishra07@users.noreply.github.com>
2022-02-13 22:38:41 +0530

    home

--- client/src/Pages/Home/Home.js
+++ client/src/Pages/Home/Home.js
@@ -1,9 +1,18 @@
-import React from 'react'
-
+import React from "react";
+import "./Home.css";
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div className="content">
+      <div className="settings">
+        <span style={{ fontSize: 30 }}>Quiz Settings</span>
+      </div>
+      <img
+        src="./home.png"
+        className="banner"
+        alt="quiz img"
+      />
+    </div>
+  );
+};
 
-export default Home
+export default Home;
