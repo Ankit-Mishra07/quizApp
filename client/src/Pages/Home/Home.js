514bf763fea77b68f022fe2be45e5ba50ff124f6
Ankit Mishra <90400569+Ankit-Mishra07@users.noreply.github.com>
2022-02-14 21:52:25 +0530

    Home page

--- client/src/Pages/Home/Home.js
+++ client/src/Pages/Home/Home.js
@@ -1,24 +1,59 @@
 import React from "react";
-import {TextField} from '@material-ui/core'
+import { Button, MenuItem, TextField } from "@material-ui/core";
 import "./Home.css";
+import Categories from "../../CatData/Categories";
 const Home = () => {
   return (
     <div className="content">
       <div className="settings">
         <span style={{ fontSize: 30 }}>Quiz Settings</span>
-      
-        <div className="settings__select">
-          <TextField label="Enter Your Name" variant="outlined" 
-          style={{marginBottom:25}}
-          />
-        </div>
 
+        <div className="settings__select">
+          <TextField
+            label="Enter Your Name"
+            variant="outlined"
+            style={{ marginBottom: 25 }}
+          />
+          <TextField
+            select
+            label="Select Category"
+            variant="outlined"
+            style={{ marginBottom: 30 }}
+          >
+            {Categories.map((cat) => (
+              <MenuItem key={cat.category} value={cat.value}>
+                {cat.category}
+              </MenuItem>
+            ))}
+          </TextField>
+
+          <TextField
+          select
+          label='Select Difficulty'
+          variant="outlined"
+          style={{marginBottom : 30}}
+          >
+            <MenuItem key="Easy" value="easy">
+              Easy
+            </MenuItem>
+            <MenuItem key="Medium" value="medium">
+              Medium
+            </MenuItem>
+            <MenuItem key="Hard" value="hard">
+              Hard
+            </MenuItem>
+          </TextField>
+
+          <Button
+          variant="contained"
+          color="primary"
+          size='large'
+          >
+            Start Quiz
+          </Button>
+        </div>
       </div>
-      <img
-        src="./home.png"
-        className="banner"
-        alt="quiz img"
-      />
+      <img src="./home.png" className="banner" alt="quiz img" />
     </div>
   );
 };
