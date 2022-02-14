12feabb0659f6a1eead59170386b68d49f8ace94
Ankit Mishra <90400569+Ankit-Mishra07@users.noreply.github.com>
2022-02-14 15:23:54 +0530

    small change

--- client/src/Pages/Home/Home.js
+++ client/src/Pages/Home/Home.js
@@ -1,10 +1,18 @@
 import React from "react";
+import {TextField} from '@material-ui/core'
 import "./Home.css";
 const Home = () => {
   return (
     <div className="content">
       <div className="settings">
         <span style={{ fontSize: 30 }}>Quiz Settings</span>
+      
+        <div className="settings__select">
+          <TextField label="Enter Your Name" variant="outlined" 
+          style={{marginBottom:25}}
+          />
+        </div>
+
       </div>
       <img
         src="./home.png"
