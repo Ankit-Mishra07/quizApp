646ee30e545fe51cd76b84fd8195fee9cc584ea1
Ankit Mishra <90400569+Ankit-Mishra07@users.noreply.github.com>
2022-02-19 00:29:34 +0530

    10mint work

--- client/src/App.js
+++ client/src/App.js
@@ -7,11 +7,18 @@ import Home from './Pages/Home/Home';
 import Quiz from './Pages/Quiz/Quiz';
 import Result from './Pages/Result/Result';
 import { useState } from 'react';
+import axios from 'axios'
 function App() {
    const [name, setName] = useState("")
-   const fetchQuestions = () =>{
-
-   }
+   const [questions, setQuestions] = useState();
+   const [score, setScore] = useState(0)
+   const fetchQuestions = async(category='', difficulty='') =>{
+      const {data} = await axios.get( `https://opentdb.com/api.php?amount=10${
+        category && `&category=${category}`
+      }${difficulty && `&difficulty=${difficulty}`}&type=multiple`)
+      
+      setQuestions(data.results)
+    }
   return (
     <BrowserRouter>
     <div className="app" style={{backgroundImage:"url(https://raw.githubusercontent.com/piyush-eon/Reactjs-Quiz-App/master/public/ques1.png)"}}>
@@ -19,7 +26,14 @@ function App() {
 
     <Routes>
       <Route path='/' element={<Home name={name} setName={setName} fetchQuestions={fetchQuestions}/>}></Route>
-      <Route path='/quiz' element={<Quiz/>}></Route>
+      <Route path='/quiz' element={<Quiz
+      name={name}
+      questions={questions}
+      score={score}
+      setScore={setScore}
+      setQuestions={setQuestions}
+      
+      />}></Route>
       <Route path='/result' element={<Result/>}></Route>
     </Routes>
 
--- client/src/Pages/Quiz/Quiz.js
+++ client/src/Pages/Quiz/Quiz.js
@@ -1,8 +1,20 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
-const Quiz = () => {
-  return (
-    <div>Quiz</div>
+const Quiz = ({name, score, questions, setQuestions, setScore}) => {
+  
+  const [options, setOptions] = useState()
+  const [currQues, setCurrQues] = useState(0)
+  useEffect(() => {
+    console.log(questions)
+
+    setOptions(questions && handleShuffle([questions[currQues]?.correct_answer,
+      ...questions[currQues]?.incorrect_answer])
+    )
+  },[questions])
+    return (
+    <div>
+
+    </div>
   )
 }
 
