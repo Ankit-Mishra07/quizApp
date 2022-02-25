b5ec3d37b7163faa4dc46497fca479ed68a1c971
Ankit Mishra <90400569+Ankit-Mishra07@users.noreply.github.com>
2022-02-25 22:22:09 +0530

    working on question component

---
+++ client/src/components/Question/Question.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react'
+import ErrorMessage from '../ErrorMessage/ErrorMessage'
+const Question = ({
+  currQues,
+  setCurrQues,
+  questions,
+  options,
+  correct,
+  score,
+  setScore,
+  setQuestions
+}) => {
+
+  const [selected, setSelected] = useState()
+  const [error, setError] = useState(false)
+
+  return (
+    <div>
+      <h1>Question : {currQues + 1}</h1>
+      <div className='singleQuestion'>
+        <h2>{questions[currQues].question}</h2>
+        <div className='options'>
+          {error && <ErrorMessage>{error}</ErrorMessage> }
+
+          {
+            options && options.map(i => (
+              <button>
+                {i}
+              </button>
+            ))
+          }
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Question
