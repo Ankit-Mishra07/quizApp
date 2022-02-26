37e19504c565dd8460bc9038f1f42ff92784e97b
Ankit Mishra <90400569+Ankit-Mishra07@users.noreply.github.com>
2022-02-26 22:48:15 +0530

    questions components

--- client/src/components/Question/Question.js
+++ client/src/components/Question/Question.js
@@ -1,5 +1,8 @@
 import React, { useState } from 'react'
 import ErrorMessage from '../ErrorMessage/ErrorMessage'
+import './Question.css'
+import {Button} from '@material-ui/core'
+import {useNavigate} from 'react-router-dom'
 const Question = ({
   currQues,
   setCurrQues,
@@ -14,8 +17,44 @@ const Question = ({
   const [selected, setSelected] = useState()
   const [error, setError] = useState(false)
 
+  const navigate = useNavigate();
+
+  const handleSelect = (i) => {
+    if(selected === i && selected===correct) {
+      return 'select';
+    }else if(selected===i && selected!==correct) {
+      return 'wrong';
+    }else if(i===correct) {
+      return 'select';
+    }
+  };
+
+  const handleCheck = (i) => {
+    setSelected(i);
+    if(i === correct) {
+      setScore(score + 1)
+    }
+    setError(false)
+  };
+
+  const handleNext = () => {
+    if(currQues>8) {
+      navigate("/result")
+    }else if(selected) {
+      setCurrQues(currQues+1)
+      setSelected()
+    } else {
+      setError("Please select an option first")
+    }
+  };
+
+  const handleQuit = () => {
+    
+  }
+
+
   return (
-    <div>
+    <div className='question'>
       <h1>Question : {currQues + 1}</h1>
       <div className='singleQuestion'>
         <h2>{questions[currQues].question}</h2>
@@ -25,14 +64,36 @@ const Question = ({
           {
             options && options.map(i => (
               <button
-              onClick={() => {}}
-              
+              onClick={() => handleCheck(i)}
+              className={`singleOption ${selected && handleSelect(i)}`}
+              key={i}
+              disabled={selected}
               >
                 {i}
               </button>
             ))
           }
         </div>
+
+          <div className='controls'>
+              <Button
+              variant='contained'
+              color='secondary'
+              size='large'
+              style={{width:185}}
+              href="/"
+              onClick={handleQuit}
+              >Quit</Button>
+              <Button
+              variant="contained"
+              color='primary'
+              size='large'
+              style={{width:185}}
+              onClick={handleNext}
+
+              >Next Question</Button>
+          </div>
+
       </div>
     </div>
   )
